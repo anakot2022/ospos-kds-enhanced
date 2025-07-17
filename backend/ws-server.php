b8312df50793a50251558834470244c74a4cd0ec
anakot2022 <96965674+anakot2022@users.noreply.github.com>
2025-07-17 17:10:58 +0700

    Update ws-server.php

--- backend/ws-server.php
+++ backend/ws-server.php
@@ -6,20 +6,25 @@ use Ratchet\ConnectionInterface;
 
 class KitchenSocket implements MessageComponentInterface {
     protected $clients;
+
     public function __construct() {
         $this->clients = new \SplObjectStorage;
     }
+
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
     }
+
     public function onMessage(ConnectionInterface $from, $msg) {
         foreach ($this->clients as $client) {
             $client->send($msg);
         }
     }
+
     public function onClose(ConnectionInterface $conn) {
         $this->clients->detach($conn);
     }
+
     public function onError(ConnectionInterface $conn, \Exception $e) {
         $conn->close();
     }
