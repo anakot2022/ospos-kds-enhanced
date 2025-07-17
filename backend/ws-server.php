9cc13b6dd769a252e22f1d3be49b0c103cb2c415
anakot2022 <96965674+anakot2022@users.noreply.github.com>
2025-07-17 16:32:43 +0700

    Create ws-server.php
    (Optional WebSocket Server)

---
+++ backend/ws-server.php
@@ -0,0 +1,31 @@
+<?php
+require 'vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class KitchenSocket implements MessageComponentInterface {
+    protected $clients;
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            $client->send($msg);
+        }
+    }
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+$server = Ratchet\App::factory('0.0.0.0', 8080);
+$server->route('/kitchen', new KitchenSocket, ['*']);
+echo "Kitchen WebSocket running on ws://0.0.0.0:8080/kitchen\n";
+$server->run();
